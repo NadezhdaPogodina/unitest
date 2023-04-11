ddc7901baa866eea0d77166c39da093c9a4c9eb2
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-04-11 21:37:43 +0300

    New commit

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,7 +4,12 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
-    }
+        if (amount < 1000) {
 
+            return boundary - amount % boundary;
+
+        } else {
+            return 0;
+        }
+    }
 }
